dac20c341d56ab4cff4a89228afd099ad0660bc9
juwa dickson <79535800+juwadickson@users.noreply.github.com>
2024-05-02 20:14:07 +0100

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function toggleMobileMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
-
-    const tabIcon = document.querySelector('.tab-icon');
-    tabIcon.classList.toggle('open');
-}
-
-
